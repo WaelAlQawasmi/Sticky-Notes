301a3afef692fc585adfbf88cfc9da383835d4e0
waelalq <wael.abuomr@gmail.com>
2023-04-18 22:06:29 +0200

    finish style

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,73 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+
+    <div class="container">
+      <header>
+        <h1>Notes</h1>
+        <button>+</button>
+      </header>
+        <div class="card-continer">
+          <div class="card">
+            <p>text</p>
+            <p class="date">date</p>
+          </div>
+        </div>
+      
+    </div>
   </main>
 </template>
-
 <style scoped>
+main {
+
+  width: 100vw;
+  height: 100vh;
+}
+
+.container {
+  padding: 10px;
+  max-width: 1000px;
+  margin: 0 auto;
+
+}
+.date{
+  font-size: 12.5px;
+  font-weight: bold;
+}
+
 header {
-  line-height: 1.5;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.card-continer{
+  margin:25px;
+display: flex;
+flex-wrap: wrap;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.card{
+  height: 26vh;
+  width: 10vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-left: 25px;
+  margin-bottom: 25px;
+  padding: 10px;
+  border-radius:10px;
+  background-color: aqua;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+header button {
+  /* margin-left:45vw; */
+    border: none;
+    padding: 10px;
+    width: 50px;
+    height: 50px;
+    cursor: pointer;
+    background-color: rgb(21,20,20);
+    border-radius: 100%;
+    color: white;
+    font-size: 20px;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
